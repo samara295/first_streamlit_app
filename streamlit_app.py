c379da4f58363c6f61c38aefef516a0a89b38d12
samara295 <85747122+samara295@users.noreply.github.com>
2023-04-25 15:33:54 -0400

    Update streamlit_app.py
    Added new breakfast options.

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,9 @@ streamlit.title('My Parents New Health Diner')
 
 streamlit.header('Breakfast Menu')
 
-streamlit.text('Omega3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🥣 Omega3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
