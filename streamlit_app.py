1b160209ccafb97063d2bd07ee3086f0386cc827
samara295 <85747122+samara295@users.noreply.github.com>
2023-04-25 14:29:43 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,5 @@ streamlit.title('My Parents New Health Diner')
 streamlit.header('Breakfast Menu')
 
 streamlit.text('Omega3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie)
+streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
